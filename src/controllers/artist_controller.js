d1afdf26147afd2d0925f423809b247ae47d4ac9
Shayne Torres <shaynetorres@Shaynes-MacBook-Pro.local>
2018-02-21 04:34:42 -0800

    begin adding details page for artists

--- src/controllers/artist_controller.js
+++ src/controllers/artist_controller.js
@@ -27,5 +27,33 @@ export default() => {
         });
     })
 
+    api.get("/:id",(req, res) => {
+
+        db(connection => {
+            connection.connect(err => {
+                if (err) {
+                    console.log("There was an error: ",err);
+                    res.json({"data":[],"success":false})
+                }
+
+                connection.query("USE my_music_library", (dbErr, dbRes) => {
+                    if (dbErr) {
+                        console.log("Could not execute query: ",dbErr);
+                    }
+                })
+
+                const artistID = req.params.id;
+
+                connection.query(`SELECT * FROM artists where artist_id = ${artistID}`, (dbErr, dbRes1) => {
+                    connection.query(`SELECT * FROM albums where artist_id = ${dbRes1[0].artist_id}`, (dbErr, dbRes2) => {                        
+                        connection.query(`SELECT s.name as song_name, s.release_date, al.name as album_name, al.recording_co FROM songs s LEFT JOIN albums al on al.album_id = s.album_id where s.artist_id = ${dbRes1[0].artist_id}`, (dbErr, dbRes3) => {
+                            res.render("artist_detail",{artist:dbRes1[0], albumsData:dbRes2, songsData:dbRes3});
+                        })
+                    })
+                })
+            })
+        });
+    })
+
     return api
 }
